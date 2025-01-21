915352b5eabf4b4705f0220924344adafb039735
Elena Rumiantseva <116489176+elenarumi@users.noreply.github.com>
2025-01-21 17:02:12 +0000

    creación de 3 capas

---
+++ Source Packages/igu/Principal.java
@@ -0,0 +1,13 @@
+package igu;
+
+public class Principal {
+
+    public void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    // contenido de la clase
+
+    public void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ Source Packages/logica/logica.java
@@ -0,0 +1,20 @@
+package logica;
+
+import igu.Principal;
+
+public class logica {
+    public static void main(String[] args) {
+
+        Principal ventana = new Principal();
+
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        
+
+        
+
+
+
+
+    }
+}
